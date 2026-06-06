c9339b12598878bf31624432e86edee0cc4baf5c
SandaruwanWeerawardhana <weerawardana@gmail.com>
2026-06-06 16:43:35 +0530

    feat: i update user route in login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -41,7 +41,7 @@ export default function LoginPage() {
     if (user?.role === 'ADMIN') {
       router.push('/admin/dashboard');
     } else {
-      router.push('/dashboard');
+      router.push('/calendar');
     }
   };
 
