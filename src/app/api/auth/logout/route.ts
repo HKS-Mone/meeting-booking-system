03b4eb48e01a8277b0a5c222560ec712fc096cbc
SandaruwanWeerawardhana <weerawardana@gmail.com>
2026-06-07 19:01:27 +0530

    file re structure

--- src/app/api/auth/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import { authCookie } from '@/services/auth-server.service';
-
-export async function POST() {
-  const response = NextResponse.json({ success: true });
-  response.cookies.delete(authCookie.name);
-
-  return response;
-}
